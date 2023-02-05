90045cda7dfabb49b64097a66ca095291fb46480
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2023-02-05 17:51:21 +0530

    profile added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Category } from "./Category";
 interface Props {
@@ -9,6 +9,7 @@ interface Props {
   cata: string;
   link: string;
 }
+import pic from "./Ellipse4.png";
 export const Card = (prop: Props) => {
   const { image, title, desc, date, cata, link } = prop;
   return (
@@ -26,6 +27,15 @@ export const Card = (prop: Props) => {
       <Text color={"#6C757D"} fontSize={"12px"} fontWeight="400" my="10px">
         {desc}
       </Text>
+      <Flex fontSize={"12px"} fontWeight="400" gap="20px" align={"center"}>
+        <Image src={pic.src}></Image>
+        <Box>
+          <Text color={"#495057"} fontWeight="700" mb="5px">
+            By Jen
+          </Text>
+          <Text color={"#6C757D"}>Thinker</Text>
+        </Box>
+      </Flex>
     </Box>
   );
 };
